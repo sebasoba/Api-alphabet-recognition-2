7f8655f64cb457bb297e848fe554b7d6783a389c
sebasoba <76491101+sebasoba@users.noreply.github.com>
2022-01-26 15:51:33 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import  React,{Component} from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import PickImage from "./screens/Camera"
+
+export default class App extends React.Component{
+  render(){
+    return<PickImage/>
+  }
+}
---
+++ Camera.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+
+export default class PickImage extends React.Component {
+  state = {
+    image: null,
+  };
+
+  componentDidMount() {
+    this.getPermissionAsync();
+  }
+  getPermissionAsync = async () => {
+    if (Platform.OS !== 'web') {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('sorry we need camera permission to make this work');
+      }
+    }
+  };
+  uploadImage = async (uri) => {
+    const data = new FormData();
+    let filename = uri.split('/')[uri.split('/').length - 1];
+    let type = `image/${uri.split('.')[uri.split('.').length - 1]}`;
+    const fileToUpload = {
+      uri: uri,
+      name: filename,
+      type: type,
+    };
+    data.append('digit', fileToUpload);
+    fetch('https://f292a3137990.ngrok.io/predict-digit', {
+      method: 'POST',
+      body: data,
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log('Success:', result);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+  _pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        this.setState({ image: result.data });
+        this.uploadImage(result.uri);
+      }
+    } catch (E) {
+      console.log(E);
+    }
+  };
+  render() {
+    let { image } = this.state;
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button title="Pick image" onPress={this._pickImage} />
+      </View>
+    );
+  }
+}
